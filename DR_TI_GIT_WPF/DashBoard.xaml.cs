a5ddc0a33dfb88f4e4f2a15f7fe1adb0ec97f4b5
TomaszIwasnki <tomekV0oid123@gmail.com>
2024-01-22 09:31:04 +0100

    DashBoardPage created

--- DR_TI_GIT_WPF/DashBoard.xaml.cs
+++ DR_TI_GIT_WPF/DashBoard.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,22 +13,43 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Net.WebRequestMethods;
 
 namespace DR_TI_GIT_WPF
 {
-    /// <summary>
-    /// Logika interakcji dla klasy DashBoard.xaml
-    /// </summary>
-    public partial class DashBoard : Page
+    public partial class Dashboard : Page
     {
-        public DashBoard()
+        public Dashboard()
         {
             InitializeComponent();
+            Directory.CreateDirectory("saves/");
+            string[] files = Directory.GetFiles("saves/");
+            List<FileInfo> fileInfos = new List<FileInfo>();
+            foreach (string filePath in files)
+            {
+                FileInfo fileInfo = new FileInfo(filePath);
+                fileInfos.Add(fileInfo);
+            }
+            lstview.ItemsSource = fileInfos;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            NavigationService.Navigate(new SnakeGame());
+            //NavigationService.Navigate(new SnakeGame());
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //if (lstview.SelectedItem == null) return;
+            //FileInfo fileInfo = lstview.SelectedItem as FileInfo;
+            //string[] contents = System.IO.File.ReadAllText(fileInfo.FullName).Split(';');
+            //NavigationService.Navigate(new SnakeGame(
+            //        Convert.ToInt32(contents[0]),
+            //        Convert.ToInt32(contents[1]),
+            //        Convert.ToInt32(contents[2]),
+            //        Convert.ToInt32(contents[3])
+            //    ));
+
         }
     }
 }
