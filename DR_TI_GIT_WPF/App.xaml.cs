1982afc2662c84af83811b5ef1f25aa91666ea8a
DRaczek <DRaczek@S50-05>
2024-01-22 09:16:54 +0100

    Dodaj pliki projektów.

---
+++ DR_TI_GIT_WPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DR_TI_GIT_WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
